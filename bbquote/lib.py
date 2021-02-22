bd23f509cb6a4c985488a1ca2c84db0b9d6a591e
Zuza Zuber <zuza@lewagon.org>
2021-02-22 17:59:49 +0100

    change the output of the quote

--- bbquote/lib.py
+++ bbquote/lib.py
@@ -4,7 +4,7 @@ def get_quote():
 	url = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes'
 	response = requests.get(url).json()[0]
 
-	return f"'{response['quote']}' \n> {response['author']}"
+	return f"'{response['quote']}' \n> by: {response['author']}"
 
 if __name__ == "__main__":
 	print(get_quote())
